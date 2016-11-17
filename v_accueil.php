4717c8b38a566c337b6088ab057fea3bb6d37a09
RemiMDR <remguerin@hotmail.fr>
2016-11-17 11:14:02 +0100

    v_accueil

---
+++ v_accueil.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+<meta charset="utf-8">
+    <h1> Formations des entraineurs spécialistes athlétisme </h1><br/>
+    <tr>
+    <!-- Menu d'accueil : définit le cas d'utilisation et l'action -->
+		<td width=30><a href=index.php?uc=voirStage&action=voirLesStages>Voir les stages</a></td> <br/>
+        <td width=30><a href=index.php?uc=connexion&action=connexion>Connexion</a></td><br/>
+    </tr>
+
+</body>
+</html>
